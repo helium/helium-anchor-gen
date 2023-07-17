77a1705e81f300fa9a57204f66664ab440dc15f0
lthiery <thiery.louis@gmail.com>
2023-07-17 14:28:45 -0700

    copy some functions from the HPL source

---
+++ src/helium_sub_daos.rs
@@ -0,0 +1,337 @@
+use crate::voter_stake_registry::{LockupTrait, PositionV0, VotingMintConfig, VotingMintConfigV0};
+pub use ::helium_sub_daos::*;
+use anchor_lang::prelude::*;
+use std::cmp::min;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub const EPOCH_LENGTH: i64 = 24 * 60 * 60;
+
+pub fn current_epoch(unix_timestamp: i64) -> u64 {
+    (unix_timestamp / (EPOCH_LENGTH)).try_into().unwrap()
+}
+
+pub fn next_epoch_ts(unix_timestamp: i64) -> u64 {
+    (current_epoch(unix_timestamp) + 1) * u64::try_from(EPOCH_LENGTH).unwrap()
+}
+
+pub const FALL_RATE_FACTOR: u128 = 1_000_000_000_000;
+
+pub trait PrecisePosition {
+    fn voting_power_precise(
+        &self,
+        voting_mint_config: &VotingMintConfigV0,
+        curr_ts: i64,
+    ) -> Result<u128>;
+    fn voting_power_precise_locked_precise(
+        &self,
+        curr_ts: i64,
+        max_locked_vote_weight: u64,
+        lockup_saturation_secs: u64,
+    ) -> Result<u128>;
+
+    fn voting_power_precise_cliff_precise(
+        &self,
+        curr_ts: i64,
+        max_locked_vote_weight: u64,
+        lockup_saturation_secs: u64,
+    ) -> Result<u128>;
+}
+
+impl PrecisePosition for PositionV0 {
+    fn voting_power_precise(
+        &self,
+        voting_mint_config: &VotingMintConfigV0,
+        curr_ts: i64,
+    ) -> Result<u128> {
+        let baseline_vote_weight =
+            (voting_mint_config.baseline_vote_weight(self.amount_deposited_native)? as u128)
+                .checked_mul(FALL_RATE_FACTOR)
+                .unwrap();
+        let max_locked_vote_weight =
+            voting_mint_config.max_extra_lockup_vote_weight(self.amount_deposited_native)?;
+        let genesis_multiplier =
+            if curr_ts < self.genesis_end && voting_mint_config.genesis_vote_power_multiplier > 0 {
+                voting_mint_config.genesis_vote_power_multiplier
+            } else {
+                1
+            };
+
+        let locked_vote_weight = self.voting_power_precise_locked_precise(
+            curr_ts,
+            max_locked_vote_weight,
+            voting_mint_config.lockup_saturation_secs,
+        )?;
+
+        locked_vote_weight
+            .checked_add(baseline_vote_weight)
+            .unwrap()
+            .checked_mul(genesis_multiplier as u128)
+            .ok_or(error!(ErrorCode::ArithmeticError))
+    }
+
+    /// Vote power contribution from locked funds only.
+    fn voting_power_precise_locked_precise(
+        &self,
+        curr_ts: i64,
+        max_locked_vote_weight: u64,
+        lockup_saturation_secs: u64,
+    ) -> Result<u128> {
+        use crate::voter_stake_registry::LockupKind;
+
+        if self.lockup.expired(curr_ts) || (max_locked_vote_weight == 0) {
+            return Ok(0);
+        }
+
+        match self.lockup.kind {
+            LockupKind::None => Ok(0),
+            LockupKind::Cliff => self.voting_power_precise_cliff_precise(
+                curr_ts,
+                max_locked_vote_weight,
+                lockup_saturation_secs,
+            ),
+            LockupKind::Constant => self.voting_power_precise_cliff_precise(
+                curr_ts,
+                max_locked_vote_weight,
+                lockup_saturation_secs,
+            ),
+        }
+    }
+
+    fn voting_power_precise_cliff_precise(
+        &self,
+        curr_ts: i64,
+        max_locked_vote_weight: u64,
+        lockup_saturation_secs: u64,
+    ) -> Result<u128> {
+        let remaining = min(self.lockup.seconds_left(curr_ts), lockup_saturation_secs);
+        Ok((max_locked_vote_weight as u128)
+            .checked_mul(remaining as u128)
+            .unwrap()
+            .checked_mul(FALL_RATE_FACTOR)
+            .unwrap()
+            .checked_div(lockup_saturation_secs as u128)
+            .unwrap())
+    }
+}
+
+pub fn calculate_fall_rate(curr_vp: u128, future_vp: u128, num_seconds: u64) -> Option<u128> {
+    if num_seconds == 0 {
+        return Some(0);
+    }
+
+    let diff: u128 = curr_vp.checked_sub(future_vp).unwrap();
+
+    diff.checked_div(num_seconds.into())
+}
+
+#[derive(Debug)]
+pub struct VehntInfo {
+    pub has_genesis: bool,
+    pub vehnt_at_curr_ts: u128,
+    pub pre_genesis_end_fall_rate: u128,
+    pub post_genesis_end_fall_rate: u128,
+    pub genesis_end_vehnt_correction: u128,
+    pub genesis_end_fall_rate_correction: u128,
+    pub end_vehnt_correction: u128,
+    pub end_fall_rate_correction: u128,
+}
+pub fn caclulate_vhnt_info(
+    curr_ts: i64,
+    position: &PositionV0,
+    voting_mint_config: &VotingMintConfigV0,
+) -> Result<VehntInfo> {
+    use crate::voter_stake_registry::LockupKind;
+
+    let vehnt_at_curr_ts = position.voting_power_precise(voting_mint_config, curr_ts)?;
+
+    let has_genesis = position.genesis_end > curr_ts;
+    let seconds_to_genesis = if has_genesis {
+        u64::try_from(
+            position
+                .genesis_end
+                .checked_sub(curr_ts)
+                .unwrap()
+                // Genesis end is inclusive (the genesis will go away at exactly genesis end), so subtract 1 second
+                // We want to calculate the fall rates before genesis ends
+                .checked_sub(1)
+                .unwrap(),
+        )
+        .unwrap()
+    } else {
+        0
+    };
+    let seconds_from_genesis_to_end = if has_genesis {
+        u64::try_from(
+            position
+                .lockup
+                .end_ts
+                .checked_sub(position.genesis_end)
+                .unwrap(),
+        )
+        .unwrap()
+    } else {
+        position.lockup.seconds_left(curr_ts)
+    };
+    // One second before genesis end, the last moment we have the multiplier
+    let vehnt_at_genesis_end = position.voting_power_precise(
+        voting_mint_config,
+        curr_ts
+            .checked_add(i64::try_from(seconds_to_genesis).unwrap())
+            .unwrap(),
+    )?;
+    let vehnt_at_genesis_end_exact = if has_genesis {
+        position.voting_power_precise(voting_mint_config, position.genesis_end)?
+    } else {
+        position.voting_power_precise(voting_mint_config, curr_ts)?
+    };
+    let vehnt_at_position_end =
+        position.voting_power_precise(voting_mint_config, position.lockup.end_ts)?;
+
+    let pre_genesis_end_fall_rate =
+        calculate_fall_rate(vehnt_at_curr_ts, vehnt_at_genesis_end, seconds_to_genesis).unwrap();
+    let post_genesis_end_fall_rate = calculate_fall_rate(
+        vehnt_at_genesis_end_exact,
+        vehnt_at_position_end,
+        seconds_from_genesis_to_end,
+    )
+    .unwrap();
+
+    let mut genesis_end_vehnt_correction = 0;
+    let mut genesis_end_fall_rate_correction = 0;
+    if has_genesis {
+        let genesis_end_epoch_start_ts =
+            i64::try_from(current_epoch(position.genesis_end)).unwrap() * EPOCH_LENGTH;
+
+        if matches!(position.lockup.kind, LockupKind::Cliff) {
+            genesis_end_fall_rate_correction = pre_genesis_end_fall_rate
+                .checked_sub(post_genesis_end_fall_rate)
+                .unwrap();
+        }
+
+        // Subtract the genesis bonus from the vehnt.
+        // When we do this, we're overcorrecting because the fall rate (corrected to post-genesis)
+        // is also taking off vehnt for the time period between closing info start and genesis end.
+        // So add that fall rate back in.
+        // Only do this if the genesis end epoch isn't the same as the position end epoch.
+        // If these are the same, then the full vehnt at epoch start is already being taken off.
+        if matches!(position.lockup.kind, LockupKind::Constant)
+            || current_epoch(position.genesis_end) != current_epoch(position.lockup.end_ts)
+        {
+            // edge case, if the genesis end is _exactly_ the start of the epoch, getting the voting power at the epoch start
+            // will not include the genesis. When this happens, we'll miss a vehnt correction
+            if genesis_end_epoch_start_ts == position.genesis_end {
+                genesis_end_vehnt_correction = position
+                    .voting_power_precise(voting_mint_config, genesis_end_epoch_start_ts - 1)?
+                    .checked_sub(vehnt_at_genesis_end_exact)
+                    .unwrap();
+            } else {
+                genesis_end_vehnt_correction = position
+                    .voting_power_precise(voting_mint_config, genesis_end_epoch_start_ts)?
+                    .checked_sub(vehnt_at_genesis_end_exact)
+                    .unwrap()
+                    // Correction factor
+                    .checked_sub(
+                        post_genesis_end_fall_rate
+                            .checked_mul(
+                                u128::try_from(
+                                    position
+                                        .genesis_end
+                                        .checked_sub(genesis_end_epoch_start_ts)
+                                        .unwrap(),
+                                )
+                                .unwrap(),
+                            )
+                            .unwrap(),
+                    )
+                    .unwrap();
+            }
+        }
+    }
+
+    let mut end_fall_rate_correction = 0;
+    let mut end_vehnt_correction = 0;
+    if matches!(position.lockup.kind, LockupKind::Cliff) {
+        let end_epoch_start_ts =
+            i64::try_from(current_epoch(position.lockup.end_ts)).unwrap() * EPOCH_LENGTH;
+        let vehnt_at_closing_epoch_start =
+            position.voting_power_precise(voting_mint_config, end_epoch_start_ts)?;
+
+        end_vehnt_correction = vehnt_at_closing_epoch_start;
+        end_fall_rate_correction = post_genesis_end_fall_rate;
+    }
+
+    Ok(VehntInfo {
+        has_genesis,
+        pre_genesis_end_fall_rate,
+        post_genesis_end_fall_rate,
+        vehnt_at_curr_ts,
+        genesis_end_fall_rate_correction,
+        genesis_end_vehnt_correction,
+        end_fall_rate_correction,
+        end_vehnt_correction,
+    })
+}
+
+pub trait SubDaoEpochInfoV0Trait {
+    fn start_ts(&self) -> i64;
+    fn end_ts(&self) -> i64;
+}
+
+impl SubDaoEpochInfoV0Trait for SubDaoEpochInfoV0 {
+    fn start_ts(&self) -> i64 {
+        i64::try_from(self.epoch).unwrap() * EPOCH_LENGTH
+    }
+
+    fn end_ts(&self) -> i64 {
+        i64::try_from(self.epoch + 1).unwrap() * EPOCH_LENGTH
+    }
+}
+
+#[anchor_lang::error_code]
+pub enum ErrorCode {
+    #[msg("The realloc increase was too large")]
+    InvalidDataIncrease,
+
+    #[msg("Error in arithmetic")]
+    ArithmeticError,
+
+    #[msg("Utility score was already calculated for this sub dao")]
+    UtilityScoreAlreadyCalculated,
+
+    #[msg("Cannot calculate rewards until the epoch is over")]
+    EpochNotOver,
+
+    #[msg("All utility scores must be calculated before rewards can be issued")]
+    MissingUtilityScores,
+
+    #[msg("The subdao does not have a utility score")]
+    NoUtilityScore,
+
+    #[msg("Not enough veHNT")]
+    NotEnoughVeHnt,
+
+    #[msg("Lockup hasn't expired yet")]
+    LockupNotExpired,
+
+    #[msg("This staking position has already been purged")]
+    PositionAlreadyPurged,
+
+    #[msg("This position is healthy, refresh not needed")]
+    RefreshNotNeeded,
+
+    #[msg("Failed to calculate the voting power")]
+    FailedVotingPowerCalculation,
+
+    #[msg("Rewards need to be claimed in the correct epoch order")]
+    InvalidClaimEpoch,
+
+    #[msg("Epochs start after the earliest emission schedule")]
+    EpochTooEarly,
+
+    #[msg("Must calculate vehnt linearly. Please ensure the previous epoch has been completed")]
+    MustCalculateVehntLinearly,
+
+    #[msg("Cannot change a position while it is delegated")]
+    PositionChangeWhileDelegated,
+}
---
+++ src/voter_stake_registry.rs
@@ -0,0 +1,451 @@
+pub use ::voter_stake_registry::*;
+use std::cmp::min;
+
+pub const PRECISION_FACTOR: u128 = 1_000_000_000_000;
+
+const SCALED_FACTOR_BASE: u64 = 1_000_000_000;
+
+use anchor_lang::prelude::*;
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub trait PositionV0Trait {
+    fn voting_power(&self, voting_mint_config: &VotingMintConfigV0, curr_ts: i64) -> Result<u64>;
+    fn voting_power_locked(
+        &self,
+        curr_ts: i64,
+        max_locked_vote_weight: u64,
+        lockup_saturation_secs: u64,
+    ) -> Result<u64>;
+    fn voting_power_cliff(
+        &self,
+        curr_ts: i64,
+        max_locked_vote_weight: u64,
+        lockup_saturation_secs: u64,
+    ) -> Result<u64>;
+    fn amount_unlocked(&self, curr_ts: i64) -> u64;
+    fn amount_locked(&self, curr_ts: i64) -> u64;
+}
+
+impl PositionV0Trait for PositionV0 {
+    /// # Voting Power Caclulation
+    ///
+    /// Returns the voting power for the position, giving locked tokens boosted
+    /// voting power that scales linearly with the lockup time.
+    ///
+    /// For each cliff-locked token, the vote weight is:
+    ///
+    /// ```
+    ///    voting_power = baseline_vote_weight
+    ///                   + lockup_duration_factor * max_extra_lockup_vote_weight
+    /// ```
+    ///
+    /// with
+    ///   - lockup_duration_factor = min(lockup_time_remaining / lockup_saturation_secs, 1)
+    ///   - the VotingMintConfig providing the values for
+    ///     baseline_vote_weight, max_extra_lockup_vote_weight, lockup_saturation_secs
+    ///
+    /// ## Cliff Lockup
+    ///
+    /// The cliff lockup allows one to lockup their tokens for a set period
+    /// of time, unlocking all at once on a given date.
+    ///
+    /// The calculation for this is straightforward and is detailed above.
+    ///
+    /// ### Decay
+    ///
+    /// As time passes, the voting power decays until it's back to just
+    /// fixed_factor when the cliff has passed. This is important because at
+    /// each point in time the lockup should be equivalent to a new lockup
+    /// made for the remaining time period.
+    ///
+    fn voting_power(&self, voting_mint_config: &VotingMintConfigV0, curr_ts: i64) -> Result<u64> {
+        let baseline_vote_weight =
+            voting_mint_config.baseline_vote_weight(self.amount_deposited_native)?;
+        let max_locked_vote_weight =
+            voting_mint_config.max_extra_lockup_vote_weight(self.amount_deposited_native)?;
+        let genesis_multiplier =
+            if curr_ts < self.genesis_end && voting_mint_config.genesis_vote_power_multiplier > 0 {
+                voting_mint_config.genesis_vote_power_multiplier
+            } else {
+                1
+            };
+
+        let locked_vote_weight = self.voting_power_locked(
+            curr_ts,
+            max_locked_vote_weight,
+            voting_mint_config.lockup_saturation_secs,
+        )?;
+
+        require_gte!(
+            max_locked_vote_weight,
+            locked_vote_weight,
+            VsrError::InternalErrorBadLockupVoteWeight
+        );
+
+        baseline_vote_weight
+            .checked_add(locked_vote_weight)
+            .unwrap()
+            .checked_mul(genesis_multiplier as u64)
+            .ok_or_else(|| error!(VsrError::VoterWeightOverflow))
+    }
+
+    /// Vote power contribution from locked funds only.
+    fn voting_power_locked(
+        &self,
+        curr_ts: i64,
+        max_locked_vote_weight: u64,
+        lockup_saturation_secs: u64,
+    ) -> Result<u64> {
+        if self.lockup.expired(curr_ts) || (max_locked_vote_weight == 0) {
+            return Ok(0);
+        }
+
+        match self.lockup.kind {
+            LockupKind::None => Ok(0),
+            LockupKind::Cliff => {
+                self.voting_power_cliff(curr_ts, max_locked_vote_weight, lockup_saturation_secs)
+            }
+            LockupKind::Constant => {
+                self.voting_power_cliff(curr_ts, max_locked_vote_weight, lockup_saturation_secs)
+            }
+        }
+    }
+
+    fn voting_power_cliff(
+        &self,
+        curr_ts: i64,
+        max_locked_vote_weight: u64,
+        lockup_saturation_secs: u64,
+    ) -> Result<u64> {
+        let remaining = min(self.lockup.seconds_left(curr_ts), lockup_saturation_secs);
+        Ok(u64::try_from(
+            (max_locked_vote_weight as u128)
+                .checked_mul(remaining as u128)
+                .unwrap()
+                .checked_div(lockup_saturation_secs as u128)
+                .unwrap(),
+        )
+        .unwrap())
+    }
+
+    fn amount_unlocked(&self, curr_ts: i64) -> u64 {
+        if self.lockup.end_ts <= curr_ts {
+            self.amount_deposited_native
+        } else {
+            0
+        }
+    }
+
+    fn amount_locked(&self, curr_ts: i64) -> u64 {
+        self.amount_deposited_native
+            .checked_sub(self.amount_unlocked(curr_ts))
+            .unwrap()
+    }
+}
+
+pub trait VotingMintConfig {
+    fn digit_shift_native(&self, amount_native: u64) -> Result<u64>;
+    fn apply_factor(base: u64, factor: u64) -> Result<u64>;
+    fn baseline_vote_weight(&self, amount_native: u64) -> Result<u64>;
+    fn max_extra_lockup_vote_weight(&self, amount_native: u64) -> Result<u64>;
+    fn in_use(&self) -> bool;
+}
+
+impl VotingMintConfig for VotingMintConfigV0 {
+    /// Converts an amount in this voting mints's native currency
+    /// to the base vote weight (without the deposit or lockup scalings)
+    /// by applying the digit_shift factor.
+    fn digit_shift_native(&self, amount_native: u64) -> Result<u64> {
+        let compute = || -> Option<u64> {
+            let val = if self.digit_shift < 0 {
+                (amount_native as u128).checked_div(10u128.pow((-self.digit_shift) as u32))?
+            } else {
+                (amount_native as u128).checked_mul(10u128.pow(self.digit_shift as u32))?
+            };
+            u64::try_from(val).ok()
+        };
+        compute().ok_or_else(|| error!(VsrError::VoterWeightOverflow))
+    }
+
+    /// Apply a factor in SCALED_FACTOR_BASE units.
+    fn apply_factor(base: u64, factor: u64) -> Result<u64> {
+        let compute = || -> Option<u64> {
+            u64::try_from(
+                (base as u128)
+                    .checked_mul(factor as u128)?
+                    .checked_div(SCALED_FACTOR_BASE as u128)?,
+            )
+            .ok()
+        };
+        compute().ok_or_else(|| error!(VsrError::VoterWeightOverflow))
+    }
+
+    /// The vote weight a deposit of a number of native tokens should have.
+    ///
+    /// This vote_weight is a component for all funds in a voter account, no
+    /// matter if locked up or not.///
+    fn baseline_vote_weight(&self, amount_native: u64) -> Result<u64> {
+        Self::apply_factor(
+            self.digit_shift_native(amount_native)?,
+            self.baseline_vote_weight_scaled_factor,
+        )
+    }
+
+    /// The maximum extra vote weight a number of locked up native tokens can have.
+    /// Will be multiplied with a factor between 0 and 1 for the lockup duration.
+    fn max_extra_lockup_vote_weight(&self, amount_native: u64) -> Result<u64> {
+        Self::apply_factor(
+            self.digit_shift_native(amount_native)?,
+            self.max_extra_lockup_vote_weight_scaled_factor,
+        )
+    }
+
+    /// Whether this voting mint is configured.
+    fn in_use(&self) -> bool {
+        self.mint != Pubkey::default()
+    }
+}
+
+pub const MAX_LOCKUP_PERIODS: u32 = 365 * 200;
+pub const MAX_LOCKUP_IN_FUTURE_SECS: i64 = 100 * 365 * 24 * 60 * 60;
+pub const SECS_PER_DAY: u64 = 86_400;
+
+pub trait LockupTrait {
+    fn new_from_periods(
+        kind: LockupKind,
+        curr_ts: i64,
+        start_ts: i64,
+        periods: u32,
+    ) -> Result<Self>
+    where
+        Self: Sized;
+    fn expired(&self, curr_ts: i64) -> bool;
+    fn total_seconds(&self) -> u64;
+    fn seconds_left(&self, curr_ts: i64) -> u64;
+    fn seconds_since_expiry(&self, curr_ts: i64) -> u64;
+}
+
+impl LockupTrait for Lockup {
+    /// Create lockup for a given period
+    fn new_from_periods(
+        kind: LockupKind,
+        curr_ts: i64,
+        start_ts: i64,
+        periods: u32,
+    ) -> Result<Self> {
+        require_gt!(
+            curr_ts + MAX_LOCKUP_IN_FUTURE_SECS,
+            start_ts,
+            VsrError::DepositStartTooFarInFuture
+        );
+        require_gte!(MAX_LOCKUP_PERIODS, periods, VsrError::InvalidLockupPeriod);
+        let period_secs = if matches!(kind, LockupKind::None) {
+            0
+        } else {
+            SECS_PER_DAY
+        };
+
+        Ok(Self {
+            kind,
+            start_ts,
+            end_ts: start_ts
+                .checked_add(
+                    i64::try_from((periods as u64).checked_mul(period_secs).unwrap()).unwrap(),
+                )
+                .unwrap(),
+        })
+    }
+
+    /// True when the lockup is finished.
+    fn expired(&self, curr_ts: i64) -> bool {
+        self.seconds_left(curr_ts) == 0
+    }
+
+    // Total seconds in lockup
+    fn total_seconds(&self) -> u64 {
+        (self.end_ts - self.start_ts) as u64
+    }
+
+    /// Number of seconds left in the lockup.
+    /// May be more than end_ts-start_ts if curr_ts < start_ts.
+    fn seconds_left(&self, mut curr_ts: i64) -> u64 {
+        if matches!(self.kind, LockupKind::Constant) {
+            curr_ts = self.start_ts;
+        }
+        if curr_ts >= self.end_ts {
+            0
+        } else {
+            (self.end_ts - curr_ts) as u64
+        }
+    }
+
+    /// Number of seconds since the lockup expired.
+    /// Returns 0 if the lockup hasn't expired
+    fn seconds_since_expiry(&self, curr_ts: i64) -> u64 {
+        if !self.expired(curr_ts) {
+            return 0;
+        }
+        (curr_ts - self.end_ts) as u64
+    }
+}
+
+#[error_code]
+pub enum VsrError {
+    // 6000 / 0x1770
+    #[msg("Exchange rate must be greater than zero")]
+    InvalidRate,
+    // 6001 / 0x1771
+    #[msg("")]
+    RatesFull,
+    // 6002 / 0x1772
+    #[msg("")]
+    VotingMintNotFound,
+    // 6003 / 0x1773
+    #[msg("")]
+    DepositEntryNotFound,
+    // 6004 / 0x1774
+    #[msg("")]
+    DepositEntryFull,
+    // 6005 / 0x1775
+    #[msg("")]
+    VotingTokenNonZero,
+    // 6006 / 0x1776
+    #[msg("")]
+    OutOfBoundsDepositEntryIndex,
+    // 6007 / 0x1777
+    #[msg("")]
+    UnusedDepositEntryIndex,
+    // 6008 / 0x1778
+    #[msg("")]
+    InsufficientUnlockedTokens,
+    // 6009 / 0x1779
+    #[msg("")]
+    UnableToConvert,
+    // 6010 / 0x177a
+    #[msg("")]
+    InvalidLockupPeriod,
+    // 6011 / 0x177b
+    #[msg("")]
+    InvalidEndTs,
+    // 6012 / 0x177c
+    #[msg("")]
+    InvalidDays,
+    // 6013 / 0x177d
+    #[msg("")]
+    VotingMintConfigIndexAlreadyInUse,
+    // 6014 / 0x177e
+    #[msg("")]
+    OutOfBoundsVotingMintConfigIndex,
+    // 6015 / 0x177f
+    #[msg("Exchange rate decimals cannot be larger than registrar decimals")]
+    InvalidDecimals,
+    // 6016 / 0x1780
+    #[msg("")]
+    InvalidToDepositAndWithdrawInOneSlot,
+    // 6017 / 0x1781
+    #[msg("")]
+    ShouldBeTheFirstIxInATx,
+    // 6018 / 0x1782
+    #[msg("")]
+    ForbiddenCpi,
+    // 6019 / 0x1783
+    #[msg("")]
+    InvalidMint,
+    // 6020 / 0x1784
+    #[msg("")]
+    DebugInstruction,
+    // 6021 / 0x1785
+    #[msg("")]
+    ClawbackNotAllowedOnDeposit,
+    // 6022 / 0x1786
+    #[msg("")]
+    DepositStillLocked,
+    // 6023 / 0x1787
+    #[msg("")]
+    InvalidAuthority,
+    // 6024 / 0x1788
+    #[msg("")]
+    InvalidTokenOwnerRecord,
+    // 6025 / 0x1789
+    #[msg("")]
+    InvalidRealmAuthority,
+    // 6026 / 0x178a
+    #[msg("")]
+    VoterWeightOverflow,
+    // 6027 / 0x178b
+    #[msg("")]
+    LockupSaturationMustBePositive,
+    // 6028 / 0x178c
+    #[msg("")]
+    VotingMintConfiguredWithDifferentIndex,
+    // 6029 / 0x178d
+    #[msg("")]
+    InternalProgramError,
+    // 6030 / 0x178e
+    #[msg("")]
+    InsufficientLockedTokens,
+    // 6031 / 0x178f
+    #[msg("")]
+    MustKeepTokensLocked,
+    // 6032 / 0x1790
+    #[msg("")]
+    InvalidLockupKind,
+    // 6033 / 0x1791
+    #[msg("")]
+    InvalidChangeToClawbackDepositEntry,
+    // 6034 / 0x1792
+    #[msg("")]
+    InternalErrorBadLockupVoteWeight,
+    // 6035 / 0x1793
+    #[msg("")]
+    DepositStartTooFarInFuture,
+    // 6036 / 0x1794
+    #[msg("")]
+    VaultTokenNonZero,
+    // 6037 / 0x1795
+    #[msg("")]
+    InvalidTimestampArguments,
+
+    #[msg("Cast vote is not allowed on update_voter_weight_record_v0 endpoint")]
+    CastVoteIsNotAllowed,
+
+    #[msg("Program id was not what was expected")]
+    InvalidProgramId,
+
+    #[msg("")]
+    InvalidMintOwner,
+    #[msg("")]
+    InvalidMintAmount,
+    #[msg("")]
+    DuplicatedNftDetected,
+    #[msg("")]
+    InvalidTokenOwnerForVoterWeightRecord,
+    #[msg("")]
+    NftAlreadyVoted,
+    #[msg("")]
+    InvalidProposalForNftVoteRecord,
+    #[msg("")]
+    InvalidTokenOwnerForNftVoteRecord,
+    #[msg("")]
+    UninitializedAccount,
+    #[msg("")]
+    PositionNotWritable,
+    #[msg("")]
+    InvalidVoteRecordForNftVoteRecord,
+    #[msg("")]
+    VoteRecordMustBeWithdrawn,
+    #[msg("")]
+    VoterWeightRecordMustBeExpired,
+    #[msg("")]
+    InvalidMintForPosition,
+    #[msg("")]
+    InvalidOwner,
+    #[msg("You may not deposit additional tokens on a position created during the genesis period that still has the genesis multiplier")]
+    NoDepositOnGenesisPositions,
+    #[msg("Cannot change a position while active votes exist")]
+    ActiveVotesExist,
+    #[msg("Position update authority must sign off on this transaction")]
+    UnauthorizedPositionUpdateAuthority,
+    #[msg("Cannot transfer to the same position")]
+    SamePosition,
+}
