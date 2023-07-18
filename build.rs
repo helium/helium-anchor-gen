b558c00a837c68f55a52e23d0fea1a679a08c392
lthiery <thiery.louis@gmail.com>
2023-07-17 17:02:50 -0700

    add IDL for spl-token

---
+++ build.rs
@@ -0,0 +1,65 @@
+use std::io::Result;
+
+#[cfg(feature = "services")]
+const SERVICES: &[&str] = &[
+    "src/service/router.proto",
+    "src/service/state_channel.proto",
+    "src/service/local.proto",
+    "src/service/gateway.proto",
+    "src/service/transaction.proto",
+    "src/service/follower.proto",
+    "src/service/poc_mobile.proto",
+    "src/service/poc_lora.proto",
+    "src/service/poc_entropy.proto",
+    "src/service/packet_router.proto",
+    "src/service/iot_config.proto",
+    "src/service/mobile_config.proto",
+    "src/service/downlink.proto",
+    "src/service/multi_buy.proto",
+    "src/service/packet_verifier.proto",
+    "src/service/mapper.proto",
+];
+
+const MESSAGES: &[&str] = &[
+    "src/blockchain_txn.proto",
+    "src/entropy.proto",
+    "src/data_rate.proto",
+    "src/region.proto",
+    "src/reward_manifest.proto",
+    "src/blockchain_region_param_v1.proto",
+    "src/price_report.proto",
+    "src/mapper.proto",
+];
+
+macro_rules! config {
+    ($config:expr) => {
+        $config
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+            .field_attribute(
+                ".helium.tagged_spreading.region_spreading",
+                "#[serde(with = \"serde_region_spreading\" )]",
+            )
+    };
+}
+
+#[cfg(feature = "services")]
+fn main() -> Result<()> {
+    config!(tonic_build::configure())
+        .build_server(true)
+        .build_client(true)
+        .compile(
+            &MESSAGES
+                .iter()
+                .chain(SERVICES)
+                .copied()
+                .collect::<Vec<&str>>(),
+            &["src"],
+        )?;
+    Ok(())
+}
+
+#[cfg(not(feature = "services"))]
+fn main() -> Result<()> {
+    config!(prost_build::Config::new()).compile_protos(MESSAGES, &["src"])?;
+    Ok(())
+}
---
+++ programs/spl-token/src/lib.rs
@@ -0,0 +1,2 @@
+anchor_gen::generate_cpi_crate!("../../idl/spl_token.json");
+declare_id!("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
