f0b0c3279357bf214099ed062fb149db12d78ae1
Marc Nijdam <marc_nijdam@mac.com>
2024-11-11 17:00:18 -0800

    Chew glass and use his fork of anchor gen for enum
    anchor-gen proper does not support enum with nested field definitions. Use Chewingglass’ fork to get that feature

--- programs/helium-entity-manager/src/lib.rs
+++ programs/helium-entity-manager/src/lib.rs
@@ -1,2 +1,13 @@
 anchor_gen::generate_cpi_crate!("../../idl/helium_entity_manager.json");
 declare_id!("hemjuPXBpNvggtaUnN1MwT3wrdhttKEfosTcc2P9Pg8");
+
+impl Default for ConfigSettingsV0 {
+    fn default() -> Self {
+        ConfigSettingsV0::IotConfig {
+            min_gain: 0,
+            max_gain: 10000000,
+            full_location_staking_fee: 0,
+            dataonly_location_staking_fee: 0,
+        }
+    }
+}
--- programs/treasury-management/src/lib.rs
+++ programs/treasury-management/src/lib.rs
@@ -1,2 +1,9 @@
 anchor_gen::generate_cpi_crate!("../../idl/treasury_management.json");
 declare_id!("treaf4wWBBty3fHdyBpo35Mz84M8k3heKXmjmi9vFt5");
+
+impl Default for Curve {
+    fn default() -> Self {
+        // Default: Linear
+        Curve::ExponentialCurveV0 { k: 1 }
+    }
+}
