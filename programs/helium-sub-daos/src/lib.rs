3036b33793cfe54b20ab24761677493510d5bd50
Marc Nijdam <marc.nijdam@gmail.com>
2024-11-13 16:20:32 -0800

    Chew glass and use his fork of anchor gen for enum (#58)
    anchor-gen proper does not support enum with nested field definitions. Use Chewingglass’ fork to get that feature

--- programs/helium-sub-daos/src/lib.rs
+++ programs/helium-sub-daos/src/lib.rs
@@ -1,2 +1,8 @@
 anchor_gen::generate_cpi_crate!("../../idl/helium_sub_daos.json");
 declare_id!("hdaoVTCqhfHHo75XdAMxBKdUqvq1i5bF23sisBqVgGR");
+
+impl Default for Curve {
+    fn default() -> Self {
+        Curve::ExponentialCurveV0 { k: 1 }
+    }
+}
